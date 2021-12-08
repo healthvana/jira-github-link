0628cc5f8be4eb1f924196cab3b5dc6478ee1a9a
Ian Evanoff <ianevanoff@gmail.com>
2021-12-08 15:25:29 -0600

    Initial commits

---
+++ index.js
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch';
+
+
+
+const getIssueKeyfromBranch = async branch => {
+  const projects = await getProjects();
+  const projectsRegex = `((${projects.join('|')})-\\d{1,})`;
+  const matches = branch.match(new RegExp(projectsRegex));
+  if (!matches.length) {
+    return new Error(`No issue keys found in branch name "${branch}"`);
+  }
+  return matches[0];
+};
+
+const jiraFetch = async url => {
+  const encodedString = Buffer.from(
+    `${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}`
+  ).toString('base64');
+
+  const response = await fetch(`${JIRA_BASE_URL}${url}`, {
+    headers: {
+      Authorization: `Basic ${encodedString}`,
+    },
+  });
+  return await response.json();
+};
+
+const getProjects = async () => {
+  const projects = await jiraFetch('/rest/api/2/project/search');
+  return projects.values.map(project => project.key);
+};
+
+const getIssue = async issue => {
+  return await jiraFetch(`rest/api/2/issue/${issue}?expand=names`);
+};
+
+const getIssueInfoFromBranchName = async branch => {
+  const key = await getIssueKeyfromBranch(GITHUB_BRANCH);
+  const issueData = await getIssue(key);
+  console.log(issueData)
+}
+
+getIssueInfoFromBranchName();
