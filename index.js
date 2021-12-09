e939501b8285da1d20634c63019db20125703568
Ian Evanoff <ian@healthvana.com>
2021-12-09 10:21:17 -0600

    Yay we got all the issues

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 import fetch from 'node-fetch';
 import core from '@actions/core';
-import github from '@actions/github';
+// import github from '@actions/github';
+
+// ----FOR LOCAL DEV
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const {
+  JIRA_API_TOKEN,
+  JIRA_USER_EMAIL,
+  JIRA_BASE_URL,
+} = require('./devconfig.json');
+const github = {
+  context: {
+    payload: {
+      pull_request: {
+        title: 'PLAN-5, HV-2897 - something',
+        head: {
+          ref: 'i/PLAN-5/akjshdkjh',
+        },
+      },
+    },
+  },
+};
+//
 
 const context = github.context;
 
-const { JIRA_API_TOKEN, JIRA_USER_EMAIL, JIRA_BASE_URL } = process.env;
+// --- FOR PROD
+// const { JIRA_API_TOKEN, JIRA_USER_EMAIL, JIRA_BASE_URL } = process.env;
+//
 
-const getIssueKeyfromBranch = async () => {
-  //branch
-  const {
-    payload: { pull_request },
-  } = context;
-
-  console.log(pull_request);
-
-  // const {
-  //   title,
-  //   head: { ref: branch },
-  // } = pull_request;
-  // Get every possible project key from Jira
-  // const projects = await getProjects();
-  // //Look for possible keys using this regex
-  // const projectsRegex = `((${projects.join('|')})-\\d{1,})`;
-  // const branchMatches = branch.match(new RegExp(projectsRegex), 'gi');
-  // const titleMatches = title.match(new RegExp(projectsRegex));
-  // if (!branchMatches?.length && !titleMatches?.length) {
-  //   return new Error(`No issue keys found in branch name "${branch}"`);
-  // }
-  // const issues = [];
-  // // evaluate both and return best
-
-  // return branchMatches[0];
-};
-
+// UTIL - Authed Jira fetch function
+/**
+ *
+ * @param {string} url
+ * @returns {object}
+ */
 const jiraFetch = async url => {
   const encodedString = Buffer.from(
     `${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}`
@@ -46,21 +49,77 @@ const jiraFetch = async url => {
   return await response.json();
 };
 
+/**
+ * Gets all existing projects from Jira
+ * @returns {array}
+ */
 const getProjects = async () => {
   const projects = await jiraFetch('/rest/api/2/project/search');
   return projects.values.map(project => project.key);
 };
 
+/**
+ * Gets information on a specific JIRA issue
+ * @param {string} issue Jira issue key
+ * @returns {object} issue information
+ */
 const getIssue = async issue => {
   return await jiraFetch(`rest/api/2/issue/${issue}?expand=names`);
 };
 
-// const getIssueInfoFromBranchName = async () => {
-//   const key = await getIssueKeyfromBranch();
-//   const issueData = await getIssue(key);
-//   core.setOutput('issueData', issueData);
-//   console.log(context);
-//   return issueData;
-// };
+// ----------WORKFLOW
+/**
+ * Uses the context from a github action to take
+ * the title and branch name in question and pull out
+ * all Jira Project keys.
+ * @returns {array} Array of issue keys
+ */
+const getIssueKeysfromBranch = async () => {
+  // Get PR info from Github Action context
+  const {
+    payload: {
+      pull_request: {
+        title,
+        head: { ref: branch },
+      },
+    },
+  } = context;
 
-getIssueKeyfromBranch();
+  // Get every possible project key from Jira
+  const projects = await getProjects();
+  // Look for possible keys using this regex
+  const projectsRegex = `((${projects.join('|')})-\\d{1,})`;
+  const regexp = new RegExp(projectsRegex, 'gi');
+  const branchMatches = branch.match(regexp);
+  const titleMatches = title.match(regexp);
+  // If none, throw; maybe later comment on the PR?
+  if (!branchMatches?.length && !titleMatches?.length) {
+    return new Error(`No issue keys found in branch name "${branch}"`);
+  }
+  return [...new Set(branchMatches.concat(titleMatches))];
+};
+
+const getIssueInfoFromBranchName = async () => {
+  const keys = await getIssueKeysfromBranch();
+  console.log('keys::', keys);
+  const issuesData = await Promise.all(
+    keys.map(async key => {
+      let data = null;
+      try {
+        data = await getIssue(key);
+      } catch (e) {
+        console.error(
+          `Issue ${key} could not be found in Jira or could not be fetched:`
+        );
+        console.error(e);
+      }
+      return data;
+    })
+  );
+
+  // core.setOutput('issuesData', issuesData);
+  console.log(issuesData);
+  return issuesData;
+};
+
+getIssueInfoFromBranchName();
